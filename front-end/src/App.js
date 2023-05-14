bba4326b25d09cf5c277b76adfe8f938f7b7c215
jwong65 <jason.wong6507@gmail.com>
2023-05-14 11:06:10 -0400

    Moved into src directory

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 // import './App.css';
 
-import '../scss/styles.scss'
+import './scss/styles.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
