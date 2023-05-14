f53501a1b8b36cfe96b735853b9e883fa5f9ec83
jwong65 <jason.wong6507@gmail.com>
2023-05-14 13:17:33 -0400

    added tracker page and added scss for both

---
+++ front-end/src/pages/Tracker.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Tracker() {
+  return (
+    <div className='main'>
+        <div>
+            <form>
+                
+            </form>
+        </div>
+
+    </div>
+  )
+}
