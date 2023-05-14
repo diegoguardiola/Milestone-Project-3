a14500261c58bf0ff5bdcaeccacc15ee8cec2906
JustSlamThings <justslamthings@gmail.com>
2023-05-14 15:51:35 -0400

    tweaked the sass

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './css/styles.css';
+import './css/signup.css';
+import './css/tracker.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
