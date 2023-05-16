ca2c316870162f48aa67e3e1339af180ec7ec793
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2023-05-16 18:00:28 -0400

    signup form

--- front-end/src/pages/Signup.js
+++ front-end/src/pages/Signup.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import '../scss/styles.scss'
 
-export default function Signup() {
-  return (
-    <div>Signup</div>
+export default function SignUpForm() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('Submitted:', firstName, lastName, email, password)
+  }
+
+  return(
+    <form onSubmit = {handleSubmit}>
+      <label>
+        First Name:
+        <input type = "text" value = {firstName} onChange={(e) => setFirstName(e.target.value)}/>
+      </label>
+      <br/>
+      <label>
+        Last Name:
+        <input type = "text" value = {lastName} onChange={(e) => setLastName(e.target.value)}/>
+      </label>
+      <br/>
+      <label>
+        Email:
+        <input type = "email" value = {email} onChange={(e) => setEmail(e.target.value)}/>
+      </label>
+      <br/>
+      <label>
+        Password:
+        <input type = "password" value = {password} onChange={(e) => setPassword(e.target.value)}/>
+      </label>
+      <br/>
+  <button type = "submit">Submit</button>
+    </form>
   )
 }
+
