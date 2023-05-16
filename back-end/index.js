f6370b3696f42a832d766a9eb1d274e2a62f34f8
Diego Guardiola <diego.guardiola@falcoautomation.local>
2023-05-16 19:57:32 -0400

    changed port on the backend

--- back-end/index.js
+++ back-end/index.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.PROFILE_CONNECTION)
 })
 mongoose.set('strictQuery', false);
 
-app.listen(3000, () => {
+app.listen(5000, () => {
     console.log(api);
     console.log('sever running');
 })
