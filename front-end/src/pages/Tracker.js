4c9a1dfc2ff4378f03e6d57ebec009c57227dd85
jwong65 <jason.wong6507@gmail.com>
2023-05-18 19:39:07 -0400

    Commented out Tracker.js and readjusted Home.js

--- front-end/src/pages/Tracker.js
+++ front-end/src/pages/Tracker.js
@@ -1,23 +1,23 @@
-import React, { useContext, useState } from 'react'
-import '../css/styles.css'
+// import React, { useContext, useState } from 'react'
+// import '../css/styles.css'
 
 
-export default function Tracker() {
-  const { setCurrentUser } = useContext(CurrentUser)
-  const [credentials, setCredentials] = useState({
-    email: '',
-    password: ''
-})
-  return (
-        <CurrentUserProvider>
-    <div className='main'>
-        Hello
-        <div id='formbox'>
+// export default function Tracker() {
+//   const { setCurrentUser } = useContext(CurrentUser)
+//   const [credentials, setCredentials] = useState({
+//     email: '',
+//     password: ''
+// })
+//   return (
+//         <CurrentUserProvider>
+//     <div className='main'>
+//         Hello
+//         <div id='formbox'>
 
 
-        </div>
+//         </div>
 
-        </CurrentUserProvider>
-      );
-    }
+//         </CurrentUserProvider>
+//       );
+//     }
     
