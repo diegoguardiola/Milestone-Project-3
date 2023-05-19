7d53768eaeea517c6ef790ac0ceb923fbb333500
JustSlamThings <justslamthings@gmail.com>
2023-05-18 20:39:26 -0400

    Fixing up tracker.js

--- front-end/src/pages/Tracker.js
+++ front-end/src/pages/Tracker.js
@@ -2,41 +2,52 @@ import React, { useContext, useState } from 'react'
 import '../css/styles.css'
 
 export default function setChildData() {
-  const [signUpData, setChildDataa] = useState({
+  const { setCurrentUser } = useContext(CurrentUser)
+  const [childData, setChildData] = useState({
     SiteName: '',
     email: '',
     password: '',
   }) 
 
-export default function Tracker() {
-  const { setCurrentUser } = useContext(CurrentUser)
-  const [credentials, setCredentials] = useState({
-    SiteName: '',
-    email: '',
-    password: '',
-})
   return (
         <CurrentUserProvider>
 <div className='main'>
         Hello
-        <div id='formbox'>
+        <div>
+            <h1>Saved Login Journals</h1>
+        <table>
+          
+          
+          </table>  
+        </div>
+
+        <div id='formbox' >
+          <div>
+            <h1>Adding new login information</h1>
+          </div>
         <label>
         Site Name
-        <input type = "text" required value = {signUpData.SiteName} onChange={(e) => setChildData({ ...signUpData, SiteName: e.target.value })}/>
+        <input type = "text" required value = {setChildData.SiteName} onChange={(e) => setChildData({ ...setChildData, SiteName: e.target.value })}/>
       </label>
       <br/>
       <label>
         Email:
-        <input type = "email" required value = {signUpData.email} onChange={(e) => setChildData({ ...signUpData, email: e.target.value })}/>
+        <input type = "email" required value = {setChildData.email} onChange={(e) => setChildData({ ...setChildData, email: e.target.value })}/>
       </label>
       <br/>
       <label>
         Password:
-        <input type = "password" required value = {signUpData.masterPassword} onChange={(e) => setChildData({ ...signUpData, masterPassword: e.target.value })}/>
+        <input type = "password" required value = {setChildData.Password} onChange={(e) => setChildData({ ...setChildData, Password: e.target.value })}/>
       </label>
 
         </div>
 </div>
+
+<div className='population'>
+
+
+</div>
+
         </CurrentUserProvider>
       );
       };
