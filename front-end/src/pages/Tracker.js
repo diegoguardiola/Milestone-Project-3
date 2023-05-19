9b4359ca41119d50a6b4335a38a1e70a253119b9
JustSlamThings <justslamthings@gmail.com>
2023-05-18 20:21:45 -0400

    adding in tracker information

--- front-end/src/pages/Tracker.js
+++ front-end/src/pages/Tracker.js
@@ -1,10 +1,17 @@
 import React, { useContext, useState } from 'react'
 import '../css/styles.css'
 
+export default function setChildData() {
+  const [signUpData, setChildDataa] = useState({
+    SiteName: '',
+    email: '',
+    password: '',
+  }) 
 
 export default function Tracker() {
   const { setCurrentUser } = useContext(CurrentUser)
   const [credentials, setCredentials] = useState({
+    SiteName: '',
     email: '',
     password: '',
 })
@@ -13,7 +20,20 @@ export default function Tracker() {
 <div className='main'>
         Hello
         <div id='formbox'>
-
+        <label>
+        Site Name
+        <input type = "text" required value = {signUpData.SiteName} onChange={(e) => setChildData({ ...signUpData, SiteName: e.target.value })}/>
+      </label>
+      <br/>
+      <label>
+        Email:
+        <input type = "email" required value = {signUpData.email} onChange={(e) => setChildData({ ...signUpData, email: e.target.value })}/>
+      </label>
+      <br/>
+      <label>
+        Password:
+        <input type = "password" required value = {signUpData.masterPassword} onChange={(e) => setChildData({ ...signUpData, masterPassword: e.target.value })}/>
+      </label>
 
         </div>
 </div>
