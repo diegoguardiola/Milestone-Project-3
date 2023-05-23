b649003a2a4c197ca508ea29dd95ae72352cabd6
jwong65 <jason.wong6507@gmail.com>
2023-05-23 19:44:13 -0400

    Adding NavigationBar styling, not functional

--- front-end/src/components/NavigationBar.js
+++ front-end/src/components/NavigationBar.js
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
 
-const Navbar = () => {
+// React-Bootstrap
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+
+const Navibar = () => {
   const { logout } = useLogout();
   const { user } = useAuthContext();
 
@@ -12,27 +17,29 @@ const Navbar = () => {
   };
 
   return (
-    <header>
-      <div className="container">
-        <nav>
+    <Navbar bg='light' expand='lg'>
+      <Container>
+        <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className="me-auto">
           {user && (
             <div>
               <span>{user.email}</span>
               <button onClick={handleClick}>Log out</button>
-              <Link to="/">Home</Link>
+              <Nav.Link to="/"><Link to='/'>Home</Link></Nav.Link>
             </div>
           )}
           {!user && (
             <div>
-              <Link to="/">Home</Link>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
+              <Nav.Link to="/">Home</Nav.Link>
+              <Nav.Link to="/login">Login</Nav.Link>
+              <Nav.Link to="/signup">Signup</Nav.Link>
             </div>
           )}
-        </nav>
-      </div>
-    </header>
+        </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default Navibar;
