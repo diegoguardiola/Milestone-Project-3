568801002a76e280d613fbb271d6d809f77dcd46
Diego Guardiola <diego.guardiola@falcoautomation.local>
2023-05-23 21:19:48 -0400

    updating

---
+++ front-end/build/precache-manifest.877e841546d455dd439b867d76a16e0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba567511322bbc4fdde6",
+    "url": "/static/css/main.b444e9c4.chunk.css"
+  },
+  {
+    "revision": "ba567511322bbc4fdde6",
+    "url": "/static/js/main.0f40fa5f.chunk.js"
+  },
+  {
+    "revision": "7cc40d8d0337d3b8acbb",
+    "url": "/static/js/runtime~main.e5f67e95.js"
+  },
+  {
+    "revision": "c49d19cf192bb7e30811",
+    "url": "/static/css/2.bd625516.chunk.css"
+  },
+  {
+    "revision": "c49d19cf192bb7e30811",
+    "url": "/static/js/2.58acd6f3.chunk.js"
+  },
+  {
+    "revision": "0637dd79202ea88c332b",
+    "url": "/static/js/3.7ebd281e.chunk.js"
+  },
+  {
+    "revision": "016d907abe5f7d5ae321d589212df117",
+    "url": "/index.html"
+  }
+];
